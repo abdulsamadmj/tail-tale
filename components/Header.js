4319e1609d3cc228c332dae1ac6b6094dfb06f67
Abdul Samad M J <samadmj4@gmail.com>
2022-03-17 12:55:34 +0530

    Icon, logo transition done

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,22 @@ import React from 'react'
 function Header() {
   return (
     <div>
-        <h1>I am a Header</h1>
-
-        <div>
-            <Image 
-            src='E:\tail-tale\public\Logo-1.png'
+      <div className='flex justify-between max-w-6xl'>
+        <div className='relative h-24 hidden lg:inline-grid w-24'>
+          <Image
+            src='/../public/Logo.png'
             layout='fill'
-            />
+            objectFit='contain'
+          />
         </div>
+        <div className='relative w-10 h-10 lg:hidden flex-shrink-0'>
+        <Image
+            src='/../public/icon.png'
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+      </div>
     </div>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images:{
-    domains:['E:\tail-tale\public\Logo-1.png'],
-  },
+  // images:{
+  //   domains:['drive.google.com'],
+  // },
   reactStrictMode: true,
 }
