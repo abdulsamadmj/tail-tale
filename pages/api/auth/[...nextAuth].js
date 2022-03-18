4467b6ddcccb6d3c3091f5cf2c5a7717eeb0e91e
Abdul Samad M J <samadmj4@gmail.com>
2022-03-18 12:19:01 +0530

    Simple Google Sign in made

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -1,13 +1,23 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        // ...add more providers here
+    ],
+    pages:{
+        signIn:"/auth/signin",
+    }
+   
+    //default login screen
+    //   theme:{
+    //       logo:"url",
+    //       brandColor:"color",
+    //       colorScheme:"auto"
+    //   },
 })
